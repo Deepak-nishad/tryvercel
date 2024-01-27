d2f2a84833131e8d7ca0cd8752be82eb2dfcf7b0
Deepak-nishad <deepakkumar62559@gmail.com>
2024-01-27 23:00:35 +0530

    change db connectio

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 require("dotenv").config();
 
 const dbconnect = () => {
-    mongoose.connect(process.env.DatabaseUrl)
+    // mongoose.connect(process.env.DatabaseUrl)
+    mongoose.connect('mongodb+srv://ramanandsager57100:Deepak123@cluster0.ep4h9z1.mongodb.net/mydatabase')
         .then(() => { console.log("db connection succesfull") })
         .catch((error) => {
             console.log("Issue in db coonection");
