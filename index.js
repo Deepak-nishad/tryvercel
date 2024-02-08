2161a78848c96a90c5fb70031cef20c595427df8
Deepak-nishad <deepakkumar62559@gmail.com>
2024-02-09 00:36:54 +0530

    add routes on home page

--- index.js
+++ index.js
@@ -30,5 +30,43 @@ const dbconnect = require("./config/database");
 dbconnect();
 
 app.get("/", (req, res) => {
-    res.send(`<h1> This is home page </h1>`)
+    res.send(`
+<h1>Todo App Backend</h1>
+
+<ul>
+    <h3>Route 1</h3>
+    <li>Description: Create Todo Route.</li>
+    <li>Endpoint: http://localhost:3000/api/v1/createTodo</li>
+    <li>Method: POST</li>
+    <li>Parameters: {title: You can write title, description: Description goes here}</li>
+</ul>
+
+<ul>
+    <h3>Route 2</h3>
+    <li>Description: getAllTodo route.</li>
+    <li>Endpoint: <a href="http://localhost:3000/api/v1/getTodos">http://localhost:3000/api/v1/getTodos</a></li>
+    <li>Method: GET</li>
+</ul>
+
+<ul>
+    <h3>Route 3</h3>
+    <li>Description: Get Todo by id Route.</li>
+    <li>Endpoint: http://localhost:3000/api/v1/getTodos/mongodb_id</li>
+    <li>Method: GET</li>
+</ul>
+
+<ul>
+    <h3>Route 4</h3>
+    <li>Description: Update Todo Route.</li>
+    <li>Endpoint: http://localhost:3000/api/v1/updateTodo/id</li>
+    <li>Method: PUT</li>
+</ul>
+
+<ul>
+    <h3>Route 5</h3>
+    <li>Description: Delete Todo Route.</li>
+    <li>Endpoint: http://localhost:3000/api/v1/deleteTodo/Mongodb_Object_id</li>
+    <li>Method: DELETE</li>
+</ul>
+    `)
 })
